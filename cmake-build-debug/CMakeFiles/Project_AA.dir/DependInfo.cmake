6eb6df7b24b66d17e69ae231065a33a9ded058bb
CodingHub8 <manbil6146@gmail.com>
2025-01-02 16:30:09 +0800

    Latest

---
+++ cmake-build-debug/CMakeFiles/Project_AA.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/User/Documents/Degree/Year 1 Sem 1/Algorithm Analysis/Project/main.cpp" "CMakeFiles/Project_AA.dir/main.cpp.obj" "gcc" "CMakeFiles/Project_AA.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Project_AA.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_AA.dir/main.cpp.obj"
+  "CMakeFiles/Project_AA.dir/main.cpp.obj.d"
+  "Project_AA.exe"
+  "Project_AA.exe.manifest"
+  "Project_AA.pdb"
+  "libProject_AA.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project_AA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
